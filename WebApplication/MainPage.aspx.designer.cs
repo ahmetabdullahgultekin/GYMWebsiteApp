6154b08112fb42c40fd441b1d8fd5147eade671f
Ahmet Abdullah Gültekin <ahabgu@gmail.com>
2024-01-06 22:17:05 +0300

    Master added

--- WebApplication/MainPage.aspx.designer.cs
+++ WebApplication/MainPage.aspx.designer.cs
@@ -13,14 +13,5 @@ namespace WebApplication
 
     public partial class MainPage
     {
-
-        /// <summary>
-        /// navbar control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl navbar;
     }
 }
